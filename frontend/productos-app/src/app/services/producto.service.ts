c5815ff9c3dec004687e1667ddfa053043055c1f
carlos <carlosseble@gmail.com>
2025-04-01 11:32:19 +0200

    Conectar frontend con backend usando environment

--- frontend/productos-app/src/app/services/producto.service.ts
+++ frontend/productos-app/src/app/services/producto.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Producto } from '../models/producto.model';
+import { environment } from '../../enviroment/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
-  private apiUrl = 'http://localhost:8080/api/productos';
+  private apiUrl = `${environment.apiUrl}/productos`;
 
   constructor(private http: HttpClient) { }
 
---
+++ frontend/productos-app/src/enviroment/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://pruebatecnica-sv7p.onrender.com/api'
+  };
+  
